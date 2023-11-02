63bc6c52f9dd1f925e470f1c264cf09cd447de79
TochtliNava <tochtlinavadev@gmail.com>
2023-11-02 14:45:18 -0600

    testing

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +8,8 @@ namespace homework
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola");
+            Console.WriteLine("Dios");
         }
     }
 }
