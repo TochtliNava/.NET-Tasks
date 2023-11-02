0af37a87b4942a4b348848343190240102820d81
TochtliNava <tochtlinavadev@gmail.com>
2023-11-02 16:20:48 -0600

    third task initiated

--- homework/Program.cs
+++ homework/Program.cs
@@ -8,6 +8,31 @@ namespace homework
     {
         static void Main(string[] args)
         {
+            /**  
+             * Task 1.3
+            */
+
+            //  Takes the number of elements of the array
+            Console.WriteLine("Enter the number of elements of the array:");
+            int length = int.Parse(Console.ReadLine());
+
+            int[] baseArray = new int[length];
+            
+            for (int index = 0; index < baseArray.Length; index++)
+            {
+                Console.WriteLine($"Enter the value for the {index} index:");
+                baseArray[index] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Array entered by the user:");
+
+            foreach (var item in baseArray)
+            {
+                Console.Write($"{item.ToString()} ");
+            }
+
+            Console.WriteLine("Array filtered:");
+
         }
     }
 }
