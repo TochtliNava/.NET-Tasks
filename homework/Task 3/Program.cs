ec000bcafed13b36bb2906eda7e7376cfec3c77f
TochtliNava <tochtlinavadev@gmail.com>
2023-11-18 21:31:48 -0600

    Added tests for task

--- homework/Task 3/Program.cs
+++ homework/Task 3/Program.cs
@@ -8,16 +8,20 @@ namespace task3
         {
             Queue<int> queue = new Queue<int>(4);
             queue.Enqueue(1);
+            //  Gives an error
             //queue.Tail();
             queue.Enqueue(3);
             queue.Enqueue(5);
             queue.Enqueue(7);
+            //  Gives a size error
+            //queue.Enqueue(9);
             Queue<int> tailQueue = queue.Tail();
             tailQueue.Dequeue();
             tailQueue.Dequeue();
+            //  Gives another error
             //tailQueue.Tail();
             tailQueue.Dequeue();
-            bool isEmpty = tailQueue.IsEmpty();
+            bool isEmpty = tailQueue.IsEmpty();     //  True
             tailQueue.Dequeue();
             isEmpty = tailQueue.IsEmpty();
             tailQueue.Enqueue(6);
