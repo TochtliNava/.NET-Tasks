8b02b260a8548b11efe499cce42a90a3086eaf97
TochtliNava <tochtlinavadev@gmail.com>
2023-12-19 19:56:39 -0600

    I've left the events' part

---
+++ homework/Task 4/GenericDiagonalMatrix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_4
+{
+    internal class GenericDiagonalMatrix<T>
+    {
+        private T[] _diagonal;
+
+        public int Size
+        {
+            get { return _diagonal.Length; }
+        }
+
+        public T this[int x, int y]
+        {
+            get
+            {
+                if ((x - 1) < Size && (y - 1) < Size && (x - 1) >= 0 && (y - 1) >= 0)
+                {
+                    return x == y ? _diagonal[x - 1] : default(T);
+                }
+                throw new IndexOutOfRangeException("Index out of range");
+            }
+            set 
+            {
+                if ((x - 1) < Size && (y - 1) < Size && (x - 1) >= 0 && (y - 1) >= 0)
+                {
+                    _diagonal[x - 1] = x == y ? value : default(T);
+                }
+            }
+        }
+
+        public GenericDiagonalMatrix(int size)
+        {
+            if(size < 0)
+            {
+                throw new ArgumentException("The size can't be negative"); 
+            }
+            _diagonal = new T[size];
+        }
+    }
+}
---
+++ homework/Task 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericDiagonalMatrix<int> matrix = new GenericDiagonalMatrix<int>(4);
+            matrix[3, 3] = 3;
+            matrix[1, 3] = 4;
+            matrix[4, 7] = 2;   // Nothing
+            int valor = matrix[5, 7];   // Error
+        }
+    }
+}
